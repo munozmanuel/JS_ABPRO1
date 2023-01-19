cfce29e47b8902fe0802b1bbeb578a0a1f7cf94d
Eduardo <eduardo.barria.cabrera@gmail.com>
2023-01-18 22:36:46 -0400

    creo tabla de base de datos

---
+++ bd.js
@@ -0,0 +1,151 @@
+
+
+const productDB = {
+    "id": 1,
+    "nombre":"Atari 800XL",
+    "codigo": "A123",
+    "valor": "12.60",
+    "cantidad": "10",
+    "otros": {
+        "color": "azul",
+        "tamaño": "mediano"
+    },
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+
+
+    "id": 2,
+    "nombre":"Tetris",
+    "codigo": "T567",
+    "valor": "10.50",
+    "cantidad": "15",
+    "otros": {
+        "plataforma": "Nintendo",
+        "categoría": "juego de puzzle"
+    },
+
+
+
+
+
+    "id": 3,
+    "nombre":"Xbox",
+    "codigo": "X890",
+    "valor": "99.99",
+    "cantidad": "3",
+    "otros": {
+        "fabricante": "Microsoft",
+        "generación": "8va"
+    },
+
+
+
+
+    "id": 4,
+    "nombre":"Consola Sega",
+    "codigo": "S456",
+    "valor": "19.99",
+    "cantidad": "7",
+    "otros": {
+        "fabricante": "Sega",
+        "generación": "4ta"
+    },
+
+
+
+
+    "id": 5,
+    "nombre":"Game Boy",
+    "codigo": "G123",
+    "valor": "14.99",
+    "cantidad": "8",
+    "otros": {
+        "fabricante": "Nintendo",
+        "generación": "8va"
+    },
+
+
+
+
+
+    "id": 6,
+    "nombre":"Virtual Boy",
+    "codigo": "V456",
+    "valor": "45.50",
+    "cantidad": "2",
+    "otros": {
+        "fabricante": "Nintendo",
+        "generación": "1era"
+    },
+
+
+
+
+
+    "id": 7,
+    "nombre":"Juego Arcade",
+    "codigo": "J789",
+    "valor": "75.50",
+    "cantidad": "1",
+    "otros": {
+        "fabricante": "Atari",
+        "generación": "2da"
+    },
+
+
+
+
+    "id": 8,
+    "nombre":"NES",
+    "codigo": "N123",
+    "valor": "25.99",
+    "cantidad": "5",
+    "otros": {
+        "fabricante": "Nintendo",
+        "generación": "3ra"
+    },
+
+
+
+
+
+    "id": 9,
+    "nombre":"SNES",
+    "codigo": "S789",
+    "valor": "35.99",
+    "cantidad": "4",
+    "otros": {
+        "fabricante": "Nintendo",
+        "generación": "4ta"
+    },
+
+
+
+
+
+    "id": 10,
+    "nombre":"Walkman",
+    "codigo": "W123",
+    "valor": "16.50",
+    "cantidad": "8",
+    "otros": {
+        "fabricante": "Sony",
+        "generación": "1era"
+    },
+
+
+
+
+
+    "id": 11,
+    "nombre":"Macintosh",
+    "codigo": "M456",
+    "valor": "110.99",
+    "cantidad": "1",
+    "otros": {
+        "fabricante": "Apple",
+        "generación": "1era"
+    },
+
+};
