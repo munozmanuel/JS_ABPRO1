809757cd7d847cfe1f2c1782e907b8631bd5a1ba
Emerson Ramirez <emerson.ramirez.s@gmail.com>
2023-01-20 01:17:18 -0300

    Generador de cartas avance

---
+++ js/bd.js
@@ -0,0 +1,156 @@
+
+const productDB = [
+    {
+        "id": "0",
+        "nombre":"Atari 800XL",
+        "codigo":"A123",
+        "precio": 90000,
+        "poferta": 81000,
+        "oferta": false,
+        "img": "assets/img/atari-800xl.webp",
+        "otros": {
+            "año": "1980",
+            "categoria": "consola"
+        }
+    },
+
+    {
+        "id": "1",
+        "nombre":"Brick Game Tetris",
+        "codigo": "T567",
+        "precio": 5000,
+        "poferta": 4250,
+        "oferta": true,
+        "img": "assets/img/tetris.webp",
+        "otros": {
+            "año": "1990",
+            "categoria": "puzzle"
+        }
+    },
+
+    {
+        "id": "2",
+        "nombre":"Xbox Classic",
+        "codigo": "XBX",
+        "precio": 300000,
+        "poferta":255000,
+        "oferta": false,
+        "img": "assets/img/xbox.webp",
+        "otros": {
+            "fabricante": "Microsoft",
+            "año": "2001"
+        }
+},
+];
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+
+    // "id": 3,
+    // "nombre":"Consola Sega",
+    // "codigo": "S456",
+    // "valor": "165000",
+    // "cantidad": "7",
+    // "otros": {
+    //     "fabricante": "Sega",
+    //     "generación": "4ta"
+    // },
+
+
+
+
+    // "id": 5,
+    // "nombre":"Game Boy",
+    // "codigo": "G123",
+    // "valor": "380000",
+    // "cantidad": "8",
+    // "otros": {
+    //     "fabricante": "Nintendo",
+    //     "generación": "8va"
+    // },
+
+
+
+
+
+    // "id": 6,
+    // "nombre":"Virtual Boy",
+    // "codigo": "V456",
+    // "valor": "180000",
+    // "cantidad": "2",
+    // "otros": {
+    //     "fabricante": "Nintendo",
+    //     "generación": "1era"
+    // },
+
+
+
+
+
+    // "id": 7,
+    // "nombre":"Juego Arcade",
+    // "codigo": "J789",
+    // "valor": "30000",
+    // "cantidad": "1",
+    // "otros": {
+    //     "fabricante": "Atari",
+    //     "generación": "2da"
+    // },
+
+
+
+
+    // "id": 8,
+    // "nombre":"NES",
+    // "codigo": "N123",
+    // "valor": "230000",
+    // "cantidad": "5",
+    // "otros": {
+    //     "fabricante": "Nintendo",
+    //     "generación": "3ra"
+    // },
+
+
+
+
+
+    // "id": 9,
+    // "nombre":"SNES",
+    // "codigo": "S789",
+    // "valor": "220000",
+    // "cantidad": "4",
+    // "otros": {
+    //     "fabricante": "Nintendo",
+    //     "generación": "4ta"
+    // },
+
+
+
+
+
+    // "id": 10,
+    // "nombre":"Walkman",
+    // "codigo": "W123",
+    // "valor": "40000",
+    // "cantidad": "8",
+    // "otros": {
+    //     "fabricante": "Sony",
+    //     "generación": "1era"
+    // },
+
+
+
+
+
+    // "id": 11,
+    // "nombre":"Macintosh",
+    // "codigo": "M456",
+    // "valor": "300000",
+    // "cantidad": "1",
+    // "otros": {
+    //     "fabricante": "Apple",
+    //     "generación": "1era"
+    // },
+
+
+
